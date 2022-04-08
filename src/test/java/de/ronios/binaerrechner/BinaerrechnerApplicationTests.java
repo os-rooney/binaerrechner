2fe4f52a628ae544be388635d99aee5c56d39c60
Rooney H. Osman <dev@ronios.de>
2022-04-08 19:50:15 +0200

    initialize spring boot project

---
+++ src/test/java/de/ronios/binaerrechner/BinaerrechnerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ronios.binaerrechner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaerrechnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
