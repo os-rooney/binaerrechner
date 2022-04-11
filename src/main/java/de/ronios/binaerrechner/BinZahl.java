6a817d417ce475d11ba590f3cd12c3a038d33d8b
Rooney H. Osman <dev@ronios.de>
2022-04-11 23:06:07 +0200

    add ConvertNum service and create logic to convert binary to decimal

--- src/main/java/de/ronios/binaerrechner/BinZahl.java
+++ src/main/java/de/ronios/binaerrechner/BinZahl.java
@@ -1,9 +1,11 @@
 package de.ronios.binaerrechner;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 public class BinZahl {
-    @NotEmpty
+    @NotBlank
+    @Pattern(regexp = "[0-1]{4}", message = "Sie dürfen nur eine 4 stellige Zahl eingeben, die nur aus nullen und einsen besteht.")
     private String zahlInBin;
 
     public String getZahlInBin() {
--- src/main/java/de/ronios/binaerrechner/HomeController.java
+++ src/main/java/de/ronios/binaerrechner/HomeController.java
@@ -3,24 +3,32 @@ package de.ronios.binaerrechner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+
 @Controller
 public class HomeController {
-
+    private final ConvertNum convertNum;
 
     @Autowired
-    private ConvertNum convertNum;
+    HomeController(ConvertNum convertNum){
+        this.convertNum = convertNum;
+    }
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("binZahl", new BinZahl());
         return "index";
     }
-    @PostMapping("/result")
-    public String index(@ModelAttribute BinZahl binZahl, Model model){
+    @PostMapping("/")
+    public String index(@Valid @ModelAttribute BinZahl binZahl, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            return "index";
+        }
         model.addAttribute("binZahl", convertNum.binToDec(binZahl.getZahlInBin()));
         return "result";
     }
