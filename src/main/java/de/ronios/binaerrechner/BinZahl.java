4e001c54c433c8faa42f496d9ae0b69ca2f5b1e7
Rooney H. Osman <dev@ronios.de>
2022-04-11 20:53:19 +0200

    create DTO

---
+++ src/main/java/de/ronios/binaerrechner/BinZahl.java
@@ -0,0 +1,13 @@
+package de.ronios.binaerrechner;
+
+public class BinZahl {
+    private String binZahl;
+
+    public String getBinZahl() {
+        return binZahl;
+    }
+
+    public void setBinZahl(String binZahl) {
+        this.binZahl = binZahl;
+    }
+}
