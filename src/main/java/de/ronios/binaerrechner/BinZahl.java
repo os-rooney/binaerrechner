c514bcb3f432e40ac86bac2ca46f0e07f41ff191
Rooney H. Osman <dev@ronios.de>
2022-04-11 21:14:24 +0200

    import the validation dependency

--- src/main/java/de/ronios/binaerrechner/BinZahl.java
+++ src/main/java/de/ronios/binaerrechner/BinZahl.java
@@ -2,8 +2,11 @@ package de.ronios.binaerrechner;
 
 import org.springframework.stereotype.Service;
 
+import javax.validation.constraints.NotEmpty;
+
 @Service
 public class BinZahl {
+    @NotEmpty
     private String zahlInBin;
 
     public String getZahlInBin() {
